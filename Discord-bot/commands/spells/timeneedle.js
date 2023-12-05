c26cd6837395f450096ba7ae6bf5d8dcbe5f6fa4
conduit-works <150352117+conduit-works@users.noreply.github.com>
2023-12-05 13:02:21 +0530

    Update timeneedle.js

--- Discord-bot/commands/spells/timeneedle.js
+++ Discord-bot/commands/spells/timeneedle.js
@@ -1,13 +1,12 @@
-const { SlashCommandBuilder, Util } = require('discord.js');
+const { SlashCommandBuilder, Util } = require("discord.js");
 const Arweave = require("arweave");
 const fs = require("fs");
-const axios = require('axios');
-
+const axios = require("axios");
 
 // arweave store transaction setup
 
 // load the JWK wallet key file from disk
-const jwk = JSON.parse(fs.readFileSync('./wallet.json').toString());
+const jwk = JSON.parse(fs.readFileSync("./wallet.json").toString());
 
 // initialize arweave
 const arweave = Arweave.init({
@@ -16,84 +15,109 @@ const arweave = Arweave.init({
   protocol: "https",
 });
 
-
-
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('timeneedle')
-		.setDescription('store previous conversation')
-		.addStringOption((option) =>
-      	option
-        	.setName('number')
-        	.setDescription('number of previous messages')
-        	.setRequired(true)
+  data: new SlashCommandBuilder()
+    .setName("timeneedle")
+    .setDescription("store previous conversation")
+    .addStringOption((option) =>
+      option
+        .setName("number")
+        .setDescription("number of previous messages")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName("key").setDescription("Ecryption Key")
     ),
+  // .addStringOption(option =>
+  //   option.setName('key')
+  //     .setDescription('Ecryption Key'));
 
+  async execute(interaction) {
+    // input
+    const Ekey = interaction.options.getString("key");
 
-	async execute(interaction) {
+    const caller_username = interaction.user.username;
 
-		const caller_username =interaction.user.username;
+    // return interaction.reply('Pong!');
 
-		// return interaction.reply('Pong!');
+    // input
+    const count = interaction.options.getString("number");
+    const intcount = parseInt(count);
 
-		// input 
-		const count = interaction.options.getString('number');
-		const intcount = parseInt(count);
+    // previous x messages
+    const messages = await interaction.channel.messages.fetch({
+      limit: intcount,
+    });
 
-		// previous x messages
-		const messages = await interaction.channel.messages.fetch({ limit: intcount });
+    // console.log(messages)
+    // console.log(typeof(messages))
 
-		// console.log(messages)
-		// console.log(typeof(messages))
+    // sort and order previous x messages
+    const reversedKeys = Object(messages).reverse();
 
-		// sort and order previous x messages
-		const reversedKeys = Object(messages).reverse();
+    // persistant store variable
+    let loopout = [];
 
-		// persistant store variable
-		let loopout =[];
+    // print console previous x messages
+    for (const item of reversedKeys) {
+      // console.log(`${item[1].author.username}\n${item[1].createdTimestamp}\n${item[1].guildId}\n${item[1].channelId}\n${item[1].content}\n`)
 
-		// print console previous x messages
-		for (const item of reversedKeys) {
-			// console.log(`${item[1].author.username}\n${item[1].createdTimestamp}\n${item[1].guildId}\n${item[1].channelId}\n${item[1].content}\n`)
+      // key based encryption
+      let final_msg;
+      if (Ekey !== null) {
+        const base64 = Buffer.from(item[1].content).toString("base64");
+        final_msg = "0-0" + base64 + "+" + Ekey;
 
-			loopout.push({ 'username':item[1].author.username,'timestamp':item[1].createdTimestamp,'guildID':item[1].guildId,'channelID':item[1].channelId,'content':item[1].content })
+        // some_variable is either null or undefined
+      } else {
+        final_msg = item[1].content;
+      }
 
-			// console.log(item[1].author.username); //username
-			// console.log(item[1].createdTimestamp); //timestamp
-			// console.log(item[1].guildId); //server		
-			// console.log(item[1].channelId); //channel
-			// console.log(item[1].content); //message
-		}
+      loopout.push({
+        username: item[1].author.username,
+        timestamp: item[1].createdTimestamp,
+        guildID: item[1].guildId,
+        channelID: item[1].channelId,
+        content: final_msg,
+      });
 
-		const finalVariable = JSON.stringify(loopout);
-		// const base64finalVariable= Buffer.from(finalVariable).toString('base64');
-		const base64finalVariable= finalVariable
-		console.log(typeof(finalVariable))
-		console.log(finalVariable)
-		console.log(base64finalVariable)
+      // console.log(item[1].author.username); //username
+      // console.log(item[1].createdTimestamp); //timestamp
+      // console.log(item[1].guildId); //server
+      // console.log(item[1].channelId); //channel
+      // console.log(item[1].content); //message
+    }
 
-		
+    const finalVariable = JSON.stringify(loopout);
+    // const base64finalVariable= Buffer.from(finalVariable).toString('base64');
+    const base64finalVariable = finalVariable;
+    console.log(typeof finalVariable);
+    console.log(finalVariable);
+    console.log(base64finalVariable);
 
-		//Arweave Data post
-		const tx = await arweave.createTransaction(
-		  {
-		    data: base64finalVariable,
-		  },
-		  jwk
-		);
+    //Arweave Data post
+    const tx = await arweave.createTransaction(
+      {
+        data: base64finalVariable,
+      },
+      jwk
+    );
 
-		tx.addTag('Content-Type', 'text/plain');
+    tx.addTag("Content-Type", "text/plain");
 
-		await arweave.transactions.sign(tx, jwk);
-		arweave.transactions.post(tx).then(console.log).catch(console.log);
-		console.log(`https://arweave.net/${tx.id}`);
+    await arweave.transactions.sign(tx, jwk);
+    arweave.transactions.post(tx).then(console.log).catch(console.log);
+    console.log(`https://arweave.net/${tx.id}`);
 
-		// // store tx id on arweave
+    // // store tx id on arweave
 
-    const inputs = [{"input": {"function": "store", "username":caller_username,"tnxid":tx.id}}];
-    const functionId = "v_RDVpHFBIDpX1owrBXc59Z1-5O0BmNj8zPNwtNItnY"
+    const inputs = [
+      { input: { function: "store", username: caller_username, tnxid: tx.id } },
+    ];
+    const functionId = "v_RDVpHFBIDpX1owrBXc59Z1-5O0BmNj8zPNwtNItnY";
 
-    const req = await axios.post("https://api.mem.tech/api/transactions",
+    const req = await axios.post(
+      "https://api.mem.tech/api/transactions",
       {
         functionId: functionId,
         inputs: inputs,
@@ -102,14 +126,21 @@ module.exports = {
         headers: {
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
-    const res = req?.data.status
+    const res = req?.data.status;
 
-		const finalMSG = `content stored in https://arweave.net/${tx.id} ${res}`;
-		interaction.reply({content : finalMSG, ephemeral: true});
+    const finalMSG = `content stored in https://arweave.net/${tx.id} ${res}`;
 
-
-  }
+    try {
+      interaction.reply({ content: finalMSG, ephemeral: true });
+    } catch (e) {
+      try {
+        interaction.reply({ content: finalMSG, ephemeral: true });
+      } catch (e) {
+        console.log("Error replying");
+      }
+    }
+  },
 };
